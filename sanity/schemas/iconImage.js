9a917f0a1cd8659f75d1459dab84b12dc0848d7b
LacChe <lacche96@gmail.com>
2023-04-28 19:27:32 +0800

    readd image schema name and invert png colors

--- sanity/schemas/iconImage.js
+++ sanity/schemas/iconImage.js
@@ -4,12 +4,17 @@ export default {
     type: 'document',
     fields: [
         {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
             name: 'image',
             title: 'Image',
             type: 'image',
             options: {
                 hotspot: true
             }
-        }
+        },
     ]
 }
