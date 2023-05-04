4ef65e97b6ca12284de4e4e65252c6d917a5bd60
LacChe <lacche96@gmail.com>
2023-05-04 22:40:36 +0800

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useStateContext } from './utils/stateContext';
 import { Login, Home, AddPage, Profile, SetRange, CreateTaskType, CreateCategory } from './pages';
 import { Navbar } from './components';
